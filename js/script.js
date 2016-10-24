f6ecfe21d556718ff88b9808c200d1a36dc5cde4
nazarenocastrobay <nazacastrobay97@gmail.com>
2016-10-23 21:40:03 -0300

    Arreglos de responsive

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
 $(window).scroll(function(){
-
+	var ventana_ancho = $(window).width();
 	var wScroll = $(this).scrollTop();
+	if(ventana_ancho >= 600){
 
-	$('.logo').css({
+		
+
+		$('.logo').css({
 		'transform': 'translate(0px, ' + wScroll /3 +'%)'
-	});
-	$('.helado').css({
+		});
+		$('.helado').css({
 		'transform': 'translate(0px, -' + wScroll /35 +'%)'
-	});
+		});
+		console.log(wScroll)
+} else {
+	$('.logo').css({
+		'transform': 'translate(0px, ' + wScroll /1.7 +'%)'
+		});
+	console.log(wScroll)
+	}
+	
 });
